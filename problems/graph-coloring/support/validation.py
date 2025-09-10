43afcd4f794e1e0ca9e48a47daed8fc2a96ee48c
OMeGaBee <p.olszar@seznam.cz>
2025-09-10 14:40:04 +0200

    Handle the condition when the node color is missing in validation + test all inputs/solutions

--- problems/graph-coloring/support/validation.py
+++ problems/graph-coloring/support/validation.py
@@ -6,12 +6,11 @@ def validateColoring(inputProblemPath, inputResultPath):
     colors = IOParser.parse_solution(inputResultPath)
 
     for u, v in inputGraph.edges:
-        if(colors[u] is None or colors[v] is None):
-            return False
-
-        if(colors[u] == colors[v]):
-            return False
-
+        try:
+            if colors[u] == colors[v]:
+                return False
+        except KeyError:
+            raise ValueError(f"Missing color for node {u} or {v}")
     return True
 
 
@@ -19,16 +18,11 @@ if __name__ == "__main__":
     argParser = argparse.ArgumentParser()
     argParser.add_argument("inputProblem", type=str, help="Input file path of problem")
     argParser.add_argument("inputResult", type=str, help="Input file path of result")
-    argParser.add_argument("output", type=str, help="Output file path")
 
     args = argParser.parse_args()
 
     is_valid = validateColoring(args.inputProblem, args.inputResult)
     if is_valid:
-        with open(args.output, "w") as f:
-            f.write("Valid coloring\n")
         print("Valid coloring")
     else:
-        with open(args.output, "w") as f:
-            f.write("Invalid coloring\n")
         print("Invalid coloring")
