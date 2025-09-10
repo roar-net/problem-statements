201c7570ecb0a1182b73a9fdbf0b33a0d67ba139
OMeGaBee <p.olszar@seznam.cz>
2025-09-10 15:00:47 +0200

    Add  comments for validation

--- problems/graph-coloring/support/validation.py
+++ problems/graph-coloring/support/validation.py
@@ -1,13 +1,14 @@
 import argparse
-from parser import IOParser
+from parser import IOParser # for parsing input files
 
 def validateColoring(inputProblemPath, inputResultPath):
+    ''' Validate if the coloring in inputResultPath is valid for the graph in inputProblemPath. '''
     inputGraph = IOParser.parse2nx(inputProblemPath)
     colors = IOParser.parse_solution(inputResultPath)
 
     for u, v in inputGraph.edges:
         try:
-            if colors[u] == colors[v]:
+            if colors[u] == colors[v]:  # adjacent cannot have the same color
                 return False
         except KeyError:
             raise ValueError(f"Missing color for node {u} or {v}")
@@ -19,7 +20,7 @@ if __name__ == "__main__":
     argParser.add_argument("inputProblem", type=str, help="Input file path of problem")
     argParser.add_argument("inputResult", type=str, help="Input file path of result")
 
-    args = argParser.parse_args()
+    args = argParser.parse_args() # parse command line arguments
 
     is_valid = validateColoring(args.inputProblem, args.inputResult)
     if is_valid:
